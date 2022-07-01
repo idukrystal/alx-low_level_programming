c2d436ee9ef6b4a82d9b7448f3f94bcce9210f03
crysart <idukrystal@gmail.com>
2022-06-30 19:46:33 -0700

    0x00 fix 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	/* The program was successfull */
 	return (0);
