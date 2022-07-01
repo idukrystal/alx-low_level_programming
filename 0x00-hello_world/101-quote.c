68686b6a1a773a47ba797cb34b3d4de4a3ea9741
crysart <idukrystal@gmail.com>
2022-06-30 20:01:31 -0700

    0x00 fix 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@ int main(void)
 {
 	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	fprintf(stderr, "%s", message);
+	fprintf(stderr, "%s\n", message);
 	return (0);
 }
