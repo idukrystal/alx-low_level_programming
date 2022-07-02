b89c5e03ce2e71f0bef91f64e19ae2aeba70216c
crysart <idukrystal@gmail.com>
2022-07-01 21:23:22 -0700

    0x01 add 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - starting point of program
+ * Description: prints all posible 2 digit combination of 0-9 10 is same as 01
+ * Return: 0 if succesfull 1 if not
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			for (k = j + 1, k < 10, k++)
+			{
+				putchar(i + 48);
+				putchar(j + 48);
+				putchar(k + 48);
+				if (i == 7)
+				{
+					putchar('\n');
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
