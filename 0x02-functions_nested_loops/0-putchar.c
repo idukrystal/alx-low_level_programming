fe4e5ba91e3e41ac5377b36ff33ae83918d7b31f
crysart <idukrystal@gmail.com>
2022-07-05 07:03:42 -0700

    02 a 0 f

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,31 @@
 #include "main.h"
+#include "_putchar.c"
 
+/**
+ * main - Program's starting point
+ * Description: prints "_putchar" and moves to next line
+ * Return: 0 on success
+ */
 int main(void)
 {
 	int i;
 	char *message = "_putchar";
+
 	for (i = 0; i < strlen(message); i++)
 	{
 		_putchar(message[i]);
 	}
 	_putchar('\n');
-	return 0;
+	return (0);
 }
 
+/**
+ * len - Calculates the length of a string
+ * @string: The string to calculate the length of
+ *
+ * Deacription: Counts all characters in string untill it hits a null character
+ * Return: The length of the string
+ */
 int len(char *string)
 {
 	int i = 0;
