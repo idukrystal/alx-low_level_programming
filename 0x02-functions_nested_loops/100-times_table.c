316b984e094bd5e04a521400cb83936f60ebe2d4
crysart <idukrystal@gmail.com>
2022-07-05 15:05:09 -0700

    02 a 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints an n by n multiplication table
+ * @n: determines the limit of the multiplication table
+ */
+void print_times_table(int n)
+{
+	int i;
+	int j;
+	int multiple;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			multiple = i * j;
+			if (multiple < 10)
+			{
+				_putchar('0' + multiple);
+			}
+			else
+			{
+				_putchar('0' + (multiple / 10));
+				_putchar('0' + (multiple % 10));
+			}
+			if (j == n)
+				break;
+
+			_putchar(',');
+			_putchar(' ');
+
+			if ((multiple + i) >= 10)
+				continue;
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
