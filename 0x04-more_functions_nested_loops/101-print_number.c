7250153b2c087bbac93cbc94de4258a47a9bf609
crysart <idukrystal@gmail.com>
2022-07-07 16:16:30 -0700

    04 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
+/**
+ * print_number - prints n
+ * @n: the number to print
+ *
+ */
 void print_number(int n)
 {
 	int num = n;
 	int div = 419;
 	int tens = 10;
 
-	if(num < 0)
+	if (num < 0)
 	{
 		_putchar('-');
 
@@ -22,7 +27,7 @@ void print_number(int n)
 	}
 
 	tens /= 100;
-	for(; tens >= 1; tens /= 10)
+	for (; tens >= 1; tens /= 10)
 	{
 		div = num / tens;
 		_putchar('0' + div);
