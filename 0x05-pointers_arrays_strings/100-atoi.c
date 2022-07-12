41ce3713f1747f4f1496246c1e2d1d637909c6ee
crysart <idukrystal@gmail.com>
2022-07-12 10:49:56 -0700

    05 a 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,72 @@
+#include "main.h"
+
+int _atoi(char *s)
+{
+	int neg = 1;
+	int num = 0;
+	int len = _strlen(s);
+	int start = -1;
+	int stop =  0;
+	int tens = 1;
+	int connects = 1;
+	int i = 0;
+	int j = 0;
+
+	for (i = 0 ;  i < len; i++)
+	{
+		char c = s[i];
+		if ((c >= '0'  && c <= '9')|| (c == '+' || c == '-') )
+		{
+			if (c == '-' || c == '+')
+			{
+				while ((c == '-' || c == '+'))
+				{
+					if (c == '-')
+						neg *= -1;
+					if (c == '+')
+						neg *= 1;
+					i++;
+					c = s[i];
+				}
+				connects = (c >=  '0'  &&  c <= '9') ;
+			}
+			if (!connects)
+			{
+				neg = 1;
+				connects = 1;
+				continue;
+			}
+			start = i;
+			while (c >=  '0'  &&  c <= '9')
+			{
+				stop  = i;
+				i++;
+				c = s[i];
+			}
+			for (j = stop; j >= start; j--)
+			{
+				num += (s[j] - '0') * tens ;
+				tens *= 10;
+			}
+			break;
+		}
+	}
+	return num * neg;
+}
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to calculatr length of
+ *
+ * Return: length of string @s
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -12,5 +12,6 @@ char *_strcpy(char *dest, char *src);
 void print_array(int *a, int n);
 void puts_half(char *str);
 void puts2(char *str);
+int _atoi(char *s);
 
 #endif
