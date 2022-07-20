0ce6a1a81e36e9f726b2ebca85a1ec04ea910bbc
crysart <idukrystal@gmail.com>
2022-07-21 00:41:51 +0100

    08 a 7 ,8

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,8 @@ int _sqrt_recursion(int n);
 int is_sqrt(int, int);
 int is_prime_number(int n);
 int prime(int, int, int);
+int _strlen(char *);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 
 #endif /* MAIN_H */
