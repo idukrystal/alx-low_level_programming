6405eef1338e96fdb3ea2f5085e57609f2cbd331
crysart <idukrystal@gmail.com>
2022-07-21 00:57:20 +0100

    08 f 7,8

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,35 +1,50 @@
 #include "main.h"
 
-
 /**
- * is_palindrome - checks if a string is a palidrome
- * @s: the string
- * Return: 0 if not 1 if
+ * _strlen_recursion - size
+ * @s: pointer to string params
+ * Return: recursion
  */
-int is_palindrome(char *s)
-{
-	int i = 0;
-	int len  = _strlen_recursion(s);
 
-
-	for (; i < len / 2; i++)
-	{
-		if (s[i] != s[len - i - 1])
-			return (0);
-	}
-	return (1);
-
-}
-
-/**
- * _strlen_recursion - computes the length of a string
- * @s: the string
- *
- * Return: length of @s
- */
 int _strlen_recursion(char *s)
 {
 	if (!*s)
+	{
 		return (0);
-	return (1 + _strlen_recursion(s + 1));
+	}
+	return (1 + _strlen_recursion(++s));
+}
+
+/**
+ * p1 - palindrome
+ * @s: pointer to string
+ * @l: position
+ * Return: boolena
+ */
+
+int p1(char *s, int l)
+{
+	if (l < 1)
+	{
+		return (1);
+	}
+
+	if (*s == *(s + l))
+	{
+		return (p1(s + 1, l - 2));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - palindrome
+ * @s: pointer to string
+ * Return: recursion
+ */
+
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	return (p1(s, len - 1));
 }
--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,64 +1,30 @@
 #include "main.h"
 
 /**
- * wildcmp - checks if a string matches a regular expresuon
- * @s: the string
- * @w: the regular expression
- *
- * Return: 1 if match 0 otherwise
+ * wildcmp - Compare strings
+ * @s1: pointer to string params
+ * @s2: pointer to string params
+ * Return: 0
  */
-int wildcmp(char *s, char *w)
-{
-	char b = w[0],  a, tmp = '*';
-	int p = 0, i, j, len = _strlen_recursion(s), lenw = _strlen_recursion(w);
 
-	for (i = 0 ; i < len; i++)
-	{
-		a = s[i];
-		if (a == b)
-		{
-			b = w[++p];
-			continue;
-		}
-		else if (b == '*')
-		{
-			tmp = '*';
-			j = 0;
-			while (tmp == '*')
-			{
-				tmp = w[p + j];
-				j++;
-			}
-			if (a == tmp)
-				p += --j;
-			b = w[p];
-			if (p < (lenw  - 1))
-				b = w[++p];
-			continue;
-		}
-		else if (p != 0 && w[p - 1] == '*')
-			continue;
-		return (0);
-	}
-	p++;
-	while (p < lenw)
-	{
-		b = w[p++];
-		if (b != '*')
-			return (0);
-	}
-	return (1);
-}
-
-/**
- * _strlen_recursion - computes the length of a string
- * @s: the string
- *
- * Return: length of @s
- */
-int _strlen_recursion(char *s)
+int wildcmp(char *s1, char *s2)
 {
-	if (!*s)
-		return (0);
-	return (1 + _strlen_recursion(s + 1));
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
 }
