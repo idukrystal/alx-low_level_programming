13196294d0db01d0d7a1a254a4c00e638623482c
crysart <idukrystal@gmail.com>
2022-07-22 18:33:10 +0100

    0A f 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -17,14 +17,13 @@ int main(int argc, char **argv)
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		exit(1);
 	}
 	if (argc < 0)
 	{
 		printf("0\n");
 	}
-
-        for (i = 0; i < 5; i++)
+	for (i = 0; i < 5; i++)
 	{
 		no =  change / coins[i];
 		change -= no * coins[i];
