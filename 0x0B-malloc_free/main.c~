8b13d6c0058d642a44687809d1f79265120118a9
crysart <idukrystal@gmail.com>
2022-07-26 15:26:37 +0100

    b a 5

---
+++ 0x0B-malloc_free/main.c~
@@ -0,0 +1,7 @@
+#include "main.h"
+#include <stdio.h>
+int main(int ac, char **av)
+{
+	char * mark = argstostr(ac, av);
+	printf("%s", mark);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,7 +2,9 @@
 #define MAIN_H
 
 int _strlen(char *s);
-
+char *argstostr(int ac, char **av);
 char *str_concat(char *s1, char *s2);
+char *argstostr(int ac, char **av);
+int insert_substring(char *string, char *substring,int sub_length, int from);
 
 #endif /* MAIN_H */
