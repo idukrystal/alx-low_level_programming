91a7a8bcea2623482ab2c4feaf8a4abd9b263182
crysart <idukrystal@gmail.com>
2022-07-26 16:03:45 +0100

    pp

--- 0x0B-malloc_free/main.c
+++ 0x0B-malloc_free/main.c
@@ -1,7 +1,15 @@
 #include "main.h"
 #include <stdio.h>
-int main(int ac, char **av)
+int main(int ac, char *av[])
 {
-	char * mark = argstostr(ac, av);
-	printf("%s", mark);
+    char *s;
+
+    s = argstostr(ac, av);
+    if (s == NULL)
+    {
+        return (1);
+    }
+    printf("%s", s);
+    free(s);
+    return (0);
 }
