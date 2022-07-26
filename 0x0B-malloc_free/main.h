e3ca67f59cd3f91929d319ae795e83b9cc3d8ccd
crysart <idukrystal@gmail.com>
2022-07-26 23:17:54 +0100

    0b a 6

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,5 +6,7 @@ char *argstostr(int ac, char **av);
 char *str_concat(char *s1, char *s2);
 char *argstostr(int ac, char **av);
 int insert_substring(char *string, char *substring,int sub_length, int from);
+char **strtow(char *str);
+unsigned int count_words(char *);
 
 #endif /* MAIN_H */
