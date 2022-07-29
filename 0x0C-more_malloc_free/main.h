9771a678e08b14492e9b44759f4fb5e458e4d2ac
crysart <idukrystal@gmail.com>
2022-07-29 02:40:52 +0100

    p

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,6 +4,8 @@
 void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
-int _strlen(char *); 
+int _strlen(char *);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
 
 #endif /* main.h */
