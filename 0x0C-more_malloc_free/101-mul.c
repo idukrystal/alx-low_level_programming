8d6de7299242cc65be1bedc65fdf0f009b2d49bc
crysart <idukrystal@gmail.com>
2022-07-29 03:04:23 +0100

    mnn

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -19,9 +19,11 @@ int i, j;
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				printf("Error\n");
+				exit(98);
+			}
 		}
 
 	}
