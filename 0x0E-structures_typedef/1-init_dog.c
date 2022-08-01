2ed67a6a3d259f0cf8971810e7a3e96f9e187f1f
crysart <idukrystal@gmail.com>
2022-08-01 12:45:41 +0100

    0e a 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog - initializes a struct dog
+ * @d: a pointer to the struct dog
+ * @name: its age
+ * @age: its name
+ * @owner: its owners name;
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN
+#define  MAIN
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif /* MAIN */
