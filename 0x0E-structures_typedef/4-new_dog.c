148ba4886b3525460a569a0b74359accc373096a
crysart <idukrystal@gmail.com>
2022-08-01 22:14:34 +0100

    e f 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,7 +17,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if ((name == NULL || owner == NULL))
 		return (NULL);
 
-	dog_t *dog = malloc(sizeof(dog_t));
+	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
