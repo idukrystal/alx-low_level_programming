f17e5852e76d847399cc5614ce39fba8a3477ed8
crysart <idukrystal@gmail.com>
2022-08-03 19:14:54 +0100

    f f 1,2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,10 +10,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (!action)
+	if (!action || !array)
 		return;
 	for (i = 0; i < size; i++)
 	{
+		if (!
 		action(array[i]);
 	}
 }
