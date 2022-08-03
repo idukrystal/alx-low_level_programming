7109d81188ccecbf6e67a4ca666cd72dc2f9dfbc
crysart <idukrystal@gmail.com>
2022-08-03 21:13:58 +0100

    f a 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,6 +10,16 @@ int main(int argc, char **argv)
 {
 	char  *m = (char *)(main + 0);
 
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[i]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 	for (int i = 0; i < atoi(argv[1]); i++)
 	{
 		printf("%x ", *(m + i));
