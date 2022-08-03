927f8f0f8a0b1f7ad066ebecae3001904f354ff0
crysart <idukrystal@gmail.com>
2022-08-03 13:47:00 +0100

    F a 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,18 @@
+/**
+ * int_index - searches a array of numbers
+ * @array: the array to search
+ * @size: array size
+ * @cmp: saerch function
+ * Return: index of find
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
