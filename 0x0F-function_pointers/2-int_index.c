a1797683498d87079989eb7a8aced88a8177d6ed
crysart <idukrystal@gmail.com>
2022-08-03 19:20:26 +0100

    f f 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,8 +9,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (!array || !cmp)
-		return (1);
+	if (!array || !cmp || size < 0)
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
