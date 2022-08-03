1e9ba5a2e8c5d9ad44a9bdab298ae1fe13e106d8
crysart <idukrystal@gmail.com>
2022-08-03 13:18:12 +0100

    F a 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+/**
+ * array_iterator - passea each elment of array to function
+ * @array: the array
+ * @size: size of array
+ * @action: the function
+ *
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
