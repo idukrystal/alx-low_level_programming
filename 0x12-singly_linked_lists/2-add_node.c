067a24afbe352a3e83c9bde95fbb44f5f03d5239
crysart <idukrystal@gmail.com>
2022-08-11 12:57:37 +0100

    f 2 3

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -43,3 +43,21 @@ list_t *add_node(list_t **head, const char *str)
 	return (node);
 
 }
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to calculatr length of
+ *
+ * Return: length of string @s
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	if (s == NULL)
+		return (-1);
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,39 +1,41 @@
 #include "lists.h"
 #include <stdlib.h>
 
-list_t *add_node_end(list_t **head, const char *str){
+/**
+ * add_node_end - adds a new node to end of a linked list
+ * @head: pointer to list
+ * @str: value of new node
+ * Return: pointer to new node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
 	int len = _strlen(str), i;
 	char *name;
-	list_t *node;
-	list_t *tmp;
+	list_t *node, *tmp;
+
 	if (head == NULL)
 		exit(1);
-
-	node = malloc(sizeof *node);
+	node = malloc(sizeof(*node));
 	if (node == NULL)
-		return NULL;
+		return (NULL);
 	if (str != NULL)
 	{
 		name = malloc(sizeof(char) * len);
 		if (name == NULL)
 		{
 			free(node);
-			return(NULL);
+			return (NULL);
 		}
 	}
 	else
-	{
 		name = NULL;
-	}
 	for (i = 0; i <= len; i++)
 		name[i] = str[i];
-
 	node->str = name;
 	node->len = len;
 	node->next = NULL;
 	tmp = *head;
-
-	while (1)
+	for (;; tmp = tmp->next)
 	{
 		if (tmp == NULL)
 		{
@@ -45,7 +47,24 @@ list_t *add_node_end(list_t **head, const char *str){
 			tmp->next = node;
 			break;
 		}
-		tmp = tmp->next;
 	}
 	return (node);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to calculatr length of
+ *
+ * Return: length of string @s
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	if (s == NULL)
+		return (-1);
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
 }
