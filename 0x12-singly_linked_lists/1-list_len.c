87f7a39139a30f3846caa4932a976bf66fdd056d
crysart <idukrystal@gmail.com>
2022-08-11 12:24:01 +0100

    12 a 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * list_len - computes the length of a linked list structure l
+ * @h: the linked list
+ * Return: result;
+ */
 size_t list_len(const list_t *h)
 {
 	size_t i = 0;
