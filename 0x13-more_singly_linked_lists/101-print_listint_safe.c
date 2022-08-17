e3852eb562a3a828ae7da291529e355b0572119a
crysart <idukrystal@gmail.com>
2022-08-17 01:14:37 +0100

    a 101

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,80 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint_safe - prints a liked lists of integer
+ * @h: the linked list
+ * Return: no of nodes in tbe list
+ */
+size_t print_listint_safe(const listint_t *h)
+{
+	const listint_t *node;
+	size_t count = 0;
+	listptr_t *adds = NULL;
+
+	for (node = h; node != NULL; node = node->next)
+	{
+		if (!contains(adds, (long)node))
+		{
+			printf("[%#lx] %i\n", node, node->n);
+			add_nodeptr(&adds, (long) node);
+			count++;
+		}
+		else
+			break;
+	}
+	free_listptr(adds);
+	return (count);
+}
+
+/**
+ * add_nodeptr - adds a new node
+ * @head: pounter to list
+ * @n: value to add
+ * Return: pointer to new node
+ */
+listptr_t *add_nodeptr(listptr_t **head, const long n)
+{
+	listptr_t *node;
+
+	if (head == NULL)
+		exit(-1);
+	node = malloc(sizeof(*node));
+	if (node == NULL)
+		return (NULL);
+
+	node->p = (long) n;
+	node->next = (*head);
+	*head = node;
+	return (node);
+}
+
+/**
+ * contains - checks if
+ * @head: head
+ * @p: p
+ * Return: 1 if found else 0
+ */
+int contains(const listptr_t *head, const long p)
+{
+	const listptr_t *node;
+
+	for (node = head; node != NULL; node = node->next)
+	{
+		if (node->p == p)
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ * free_listptr - frees a lost of ints
+ * @head: starting node
+ */
+void free_listptr(listptr_t *head)
+{
+	if (head == NULL)
+		return;
+	free_listint(head->next);
+	free(head);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,7 +18,13 @@ typedef struct listint_s
 } listint_t;
 
 
-/* protorypes */
+typedef struct listpointer_s
+{
+	long p;
+	struct listpointer_s *next;
+} listptr_t;
+
+/* prototypes */
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -31,9 +37,14 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
+/* task 11 */
 listint_t *reverse_listint(listint_t **head);
 void reverse(listint_t **head, size_t len);
 int ret_and_swap(listint_t *node, int old, int new);
 int swap(listint_t *n, int val, size_t pos,size_t pos2);
+
+size_t print_listint_safe(const listint_t *head);
+int contains(const listptr_t *, const long);
+listptr_t *add_nodeptr(listptr_t **head, const long n);
 
 #endif /* LISTS_H */
