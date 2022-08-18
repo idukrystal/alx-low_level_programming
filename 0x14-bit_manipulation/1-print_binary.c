6587da1f42d42fca9acd0d4a289d8e731fa3f30c
crysart <idukrystal@gmail.com>
2022-08-18 18:15:14 +0100

    f 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,8 +7,10 @@
 void print_binary(unsigned long int c)
 {
 	int shift = sizeof(c) * 8;
-	unsigned long val = 0;
-	unsigned long s = (1) << (shift - 1);
+	unsigned long val = 1;
+	unsigned long s = val << (shift - 1);
+	val = 0;
+
 
 	if (c == 0)
 	{
