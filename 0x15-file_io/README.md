7d24296c7866c5395a22e403f358d53e6a5150d7
crysart <idukrystal@gmail.com>
2022-08-22 17:26:52 +0100

    a 0

---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+an alx project on the files i.o in c programming language 
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN
+#define MAIN
+#include <stdio.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int _putchar(char c);
+
+#endif /* MAIN */
