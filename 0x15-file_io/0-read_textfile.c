d421a844cf4cab6cb7947cc76842b9b893c445c6
crysart <idukrystal@gmail.com>
2022-08-22 19:56:35 +0100

    p

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,32 +1,36 @@
 #include "main.h"
 
 /**
- * read_textfile - prints a textfile to the screen
- * @filename: name of textfile
- * @letters: no of chars to print;
- * Return: no of chars actually printed
+ * read_textfile - prints a text fule to the terminal
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *file;
-	size_t count = 0;
-	signed char c;
+	int file_no;
+	ssize_t cpy, count;
+	char *text;
 
 	if (filename == NULL)
 		return (0);
-	file = fopen(filename, "r");
-	if (file == NULL)
-	{
-		return (0);
-	}
 
-	while ((c = getc(file)) != EOF && count < letters)
-	{
-		if (_putchar(c) == 1)
-			count++;
-		else
-			return (0);
-	}
-	fclose(file);
+	file_no  = open(filename, O_RDONLY);
+
+	if (file_no == -1)
+		return (0);
+
+	text  = malloc(sizeof(char) * (letters));
+	if (text == NULL)
+		return (0);
+
+	cpy  = read(file_no, text, letters);
+	count = write(STDOUT_FILENO, text, cpy);
+
+	close(file_no);
+
+	free(text);
+
 	return (count);
 }
