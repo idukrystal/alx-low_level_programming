ed6b1afdcbc80efbf82b49d0f38c502e66173264
crysart <idukrystal@gmail.com>
2022-10-22 05:59:05 +0100

    end

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,13 @@
+/**
+ * hash_table_set - set hash on a cpb web msner
+ * @ht: stuff
+ * @key: more stuff
+ * @value: most stuff
+ * Return: an int
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	int i;
+
+	return (i);
+}
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,12 @@
+/**
+ * hash_table_get - get hahshsns
+ * @ht: hshsb shhsbs
+ * @key: gahhahahhsha
+ * Return: shhsh zhhs shh
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	char i;
+
+	return (key);
+}
