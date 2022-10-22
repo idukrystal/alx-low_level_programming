7ce9e79885f180462d0c87c3e6a67c8c8115e2b1
crysart <idukrystal@gmail.com>
2022-10-22 04:44:01 +0100

    000

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,14 +1,17 @@
 #include "structs.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_node_t **arr;
-	hash_table_t *tabel;
+	hash_node_t **arr = NULL;
+	hash_table_t *tabel = malloc(sizeof(*tabel));
 
+	if (tabel == NULL)
+		return NULL;
 	arr = malloc(sizeof(*arr) * size);
 	if (arr == NULL)
 		return NULL;
 	tabel->array = arr;
-	
+
+	return tabel;
 }
