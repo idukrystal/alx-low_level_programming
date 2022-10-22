f6593cb32e9307ad3d4376493ddb5ee4bd3432b2
crysart <idukrystal@gmail.com>
2022-10-22 05:11:29 +0100

    gfgh

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,17 +1,25 @@
 #include <stdlib.h>
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - crrates an hash table
+ * @size: hash table size
+ * Return: pointer to new hash table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_node_t **arr = NULL;
 	hash_table_t *tabel = malloc(sizeof(*tabel));
 
 	if (tabel == NULL)
-		return NULL;
+		return (NULL);
 	arr = malloc(sizeof(*arr) * size);
 	if (arr == NULL)
-		return NULL;
+	{
+		free(tabel);
+		return (NULL);
+	}
 	tabel->array = arr;
 
-	return tabel;
+	return (tabel);
 }
