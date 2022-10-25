7f572f92ee523d1d2133eb177f023b47f51e0b75
crysart <idukrystal@gmail.com>
2022-10-25 03:19:35 +0100

    pop

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,16 +10,15 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	char *val;
-	char *ke;
+	char *val, *ke;
 	hash_node_t *new;
 
-	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
+	if ((ht == NULL ) || (key == NULL || strcmp(key, "") == 0))
 		exit(0);
 	index = key_index((const unsigned char *)key, ht->size);
 	val = malloc(sizeof(*val) * strlen(value));
 	if (val == NULL)
-		exit(0);;
+		exit(0);
 	strcpy(val, value);
 	for (new = ht->array[index]; new != NULL; new = new->next)
 	{
@@ -45,7 +44,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	strcpy(ke, key);
 	new->key = ke;
-        new->value = val;
+	new->value = val;
 	new->next = ht->array[index];
 	ht->array[index] = new;
 
