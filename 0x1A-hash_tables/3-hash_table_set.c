e3fe55872fb290ee5ba5c111d79b9f26032c3daa
crysart <idukrystal@gmail.com>
2022-10-25 01:53:54 +0100

    oppp

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *val;
 	hash_node_t *new;
 
-	if (key == NULL || strcmp(key, "") == 0)
+	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
 		exit(0);
 	index = key_index((const unsigned char *)key, ht->size);
 	new = malloc(sizeof(*new));
